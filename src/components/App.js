bafcfbda77fc2b6eda52c7b9aa4c7db3f0c15b67
nickcook <nickcook@umich.edu>
2021-08-18 14:14:23 -0400

    Setup up to contact creation

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import { ContactsProvider } from "../contexts/ContactsProvider";
+
+function App() {
+  const [id, setId] = useLocalStorage("id");
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  );
+  return id ? dashboard : <Login onIdSubmit={setId} />;
+}
+
+export default App;
